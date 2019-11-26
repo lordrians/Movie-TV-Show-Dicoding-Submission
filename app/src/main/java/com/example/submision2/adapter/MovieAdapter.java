415b34e6ba5dbeaf4966d55601e25777a5ad4585
Ferrian Redhia Pratama <ferrianrp@gmail.com>
2019-11-26 14:42:39 +0700

    Nambahin Movie Adapter Belom

--- app/src/main/java/com/example/submision2/adapter/MovieAdapter.java
+++ app/src/main/java/com/example/submision2/adapter/MovieAdapter.java
@@ -1,5 +1,39 @@
 package com.example.submision2.adapter;
 
-public class MovieAdapter {
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
 
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.submision2.object.Movie;
+
+import java.util.ArrayList;
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ListViewHolder> {
+    private ArrayList<Movie> listMovie;
+    Context mContext;
+
+    @NonNull
+    @Override
+    public ListViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MovieAdapter.ListViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class ListViewHolder extends RecyclerView.ViewHolder {
+        public ListViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
